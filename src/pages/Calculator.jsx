6202e4e31d1cbb3560e570004d16a4ce2d80c1d8
Shri <shridharbandgar982@gmail.com>
2025-10-16 23:48:54 +0530

    ui added for sum and error, Test case passed (input empty,input as a single number, input as a multiple string, new lines between numbers)

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import  './style.css'
+import React,{useState} from 'react'
+import  './styles.css'
 
 export const add = (numbers) => {
+
+  if (!numbers) return 0;
   
+  numbers = numbers.replace(/\\n/g, '\n');
+  const delimiter = /,|\n/;
+  const nums = numbers.split(delimiter).map(Number);
+
+  return nums.reduce((sum, n) => sum + n, 0);
+
 }
 const Calculator=()=> {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
+  
+  const handleSubmit = () => {
+      try {
+        const sum = add(input.trim());
+        setResult(sum);
+        setError("");
+      } catch (err) {
+        setError(err.message);
+        setResult(null);
+      }
+   };
+
+
   return (
    <div className="string-calculator-container">
       <h2>String Calculator</h2>
@@ -13,14 +37,28 @@ const Calculator=()=> {
         type="text"
         className="string-input"
         placeholder="Enter numbers"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
 
       <button
-        onClick={() => {}}
+        onClick={handleSubmit}
         className="string-button"
       >
         Calculate
       </button>
+
+      {result !== null && (
+        <p className={"string-sum"}>
+          Sum: {result}
+        </p>
+      )}
+
+      {error && (
+        <p className={"string-error"}>
+           {error}
+        </p>
+      )}
     </div>
   )
 }
